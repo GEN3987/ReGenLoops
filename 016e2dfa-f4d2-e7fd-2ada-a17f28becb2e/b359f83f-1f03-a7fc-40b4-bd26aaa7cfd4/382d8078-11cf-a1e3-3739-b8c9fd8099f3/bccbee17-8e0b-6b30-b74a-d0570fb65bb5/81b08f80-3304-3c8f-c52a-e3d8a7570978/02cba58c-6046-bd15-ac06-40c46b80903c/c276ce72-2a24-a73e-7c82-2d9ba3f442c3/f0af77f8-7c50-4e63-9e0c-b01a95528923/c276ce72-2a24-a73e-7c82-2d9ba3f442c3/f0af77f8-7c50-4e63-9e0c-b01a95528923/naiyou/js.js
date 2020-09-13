f5b32584356a51e5f5aec3b73b072e5dc0c89839
GEN3987 <66622502+GEN3987@users.noreply.github.com>
2020-09-13 11:14:01 +0900

    Add files via upload

---
+++ 016e2dfa-f4d2-e7fd-2ada-a17f28becb2e/b359f83f-1f03-a7fc-40b4-bd26aaa7cfd4/382d8078-11cf-a1e3-3739-b8c9fd8099f3/bccbee17-8e0b-6b30-b74a-d0570fb65bb5/81b08f80-3304-3c8f-c52a-e3d8a7570978/02cba58c-6046-bd15-ac06-40c46b80903c/c276ce72-2a24-a73e-7c82-2d9ba3f442c3/f0af77f8-7c50-4e63-9e0c-b01a95528923/c276ce72-2a24-a73e-7c82-2d9ba3f442c3/f0af77f8-7c50-4e63-9e0c-b01a95528923/naiyou/js.js
@@ -0,0 +1,10 @@
+function run() {
+  var daimei = document.getElementById("daimei").value;
+  var syurui = document.getElementById("syurui").value;
+  var hiduke = document.getElementById("hiduke").value;
+  var naiyou = document.getElementById("naiyou").value;
+  var url = document.getElementById("url").value;
+  var element = document.createElement("p")
+  var HTML = document.createTextNode("<div class=" + "news" + ">" + "<h2 class=" + "c-post__title" + "><!-- ▼ タイトル-->" + daimei + "</h2><!-- ▲ タイトル--><div class=" + "c-post__info" > <!--日付--><span class="c-post__date"> + hiduke + </span><!--これは放置で--><span class="c-post__info__seq">|</span><!--ニュースのジャンル--><span class="c-post__category"> + syurui + </span></div><!--br連打は初心者に付き物、放置で--><br><br><br><br><font color="#707070"><p><span style="font-size: 14pt;"><strong><br></strong></span></p><p><span style="font-size: 14pt;"><strong><!-- ▼ さっきのタイトル-->GEN3987の不注意によりサーバデータが紛失。<br><!--改行時はbrを-->これにより8/4までデータが戻りました。<!-- ▲ さっきのタイトル--></strong></span></p><p><!-- ▼ 内容--> + naiyou + <!-- ▲ 内容--></p></font><br>"<div class=" + "back" + "><a href=" + "./" + "><font size=" + "3" + ">Back to List</font></a></div>"); 
+  document.body.appendChild(element).appendChild(HTML);
+  }
