d12f6c70d67a587851f9671710f66b771cbe14d4
GEN3987 <66622502+GEN3987@users.noreply.github.com>
2020-09-13 10:07:00 +0900

    Update js.js

--- 016e2dfa-f4d2-e7fd-2ada-a17f28becb2e/b359f83f-1f03-a7fc-40b4-bd26aaa7cfd4/382d8078-11cf-a1e3-3739-b8c9fd8099f3/bccbee17-8e0b-6b30-b74a-d0570fb65bb5/81b08f80-3304-3c8f-c52a-e3d8a7570978/02cba58c-6046-bd15-ac06-40c46b80903c/js.js
+++ 016e2dfa-f4d2-e7fd-2ada-a17f28becb2e/b359f83f-1f03-a7fc-40b4-bd26aaa7cfd4/382d8078-11cf-a1e3-3739-b8c9fd8099f3/bccbee17-8e0b-6b30-b74a-d0570fb65bb5/81b08f80-3304-3c8f-c52a-e3d8a7570978/02cba58c-6046-bd15-ac06-40c46b80903c/js.js
@@ -2,10 +2,11 @@ function run() {
     var daimei = document.getElementById("daimei").value;
     var syurui = document.getElementById("syurui").value;
     var hiduke = document.getElementById("hiduke").value;
-    var naiyou = document.getElementById("niayou").value;
+    var naiyou = document.getElementById("naiyou").value;
     var url = document.getElementById("url").value;
     var tellrawv = document.createTextNode
-("<div class=" + "c-news__item" + ">" + "<a href=" + url + "><div class=" + "c-news__date" + ">" + hiduke + "</div><div class=" + "c-news__type" + ">" + syurui + "</div><div class=" + "c-news__badge" + "><span class=" + "c-badge c-badge--new" + ">NEW</span></div><div class=" + "c-news__name" + ">" + daimei + "</div></a></div>");    var element = document.createElement ("p")
+( naiyou + "<div class=" + "c-news__item" + ">" + "<a href=" + url + "><div class=" + "c-news__date" + ">" + hiduke + "</div><div class=" + "c-news__type" + ">" + syurui + "</div><div class=" + "c-news__badge" + "><span class=" + "c-badge c-badge--new" + ">NEW</span></div><div class=" + "c-news__name" + ">" + daimei + "</div></a></div>");  
+    var element = document.createElement ("p")
     document.body.appendChild(element).appendChild(tellrawv);
 }
 
